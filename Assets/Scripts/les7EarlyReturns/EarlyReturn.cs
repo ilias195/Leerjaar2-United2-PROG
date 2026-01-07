e540b22d9dff725df766ea13321e57e92bc83716
ilias195 <iliasafc@outlook.com>
2026-01-07 15:00:05 +0100

    les7-Early Returns af

---
+++ Assets/Scripts/les7EarlyReturns/EarlyReturn.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class EarlyReturn : MonoBehaviour
+{
+    public bool IsPlayerReadyToAttack(Player player)
+    {
+     if (player == null) return false;
+        
+       //Level1
+     if (!player.IsAlive) return false;
+
+        //Level2
+        if (player.AttackCooldown > 0) return false;
+
+       //Level3
+      if (player.Target == null) return false;
+                   
+           //Level4
+        if (!player.Target.IsAlive) { return false; }
+                        
+         //Level5
+     if (Vector3.Distance(player.transform.position, player.Target.transform.position) >= 5f) {  return false; }
+
+        //Level6
+        // Nog meer geneste conditions met && en ||
+        bool ManaWeapon =
+        (player.Mana >= 20 && player.WeaponEquipped);
+        ManaWeapon = true;
+
+       bool HasHealthBuff = 
+       (player.Health > 30 && player.HasBuff("Strength"));
+        HasHealthBuff = true;
+
+        if (!ManaWeapon && !HasHealthBuff) {return false;}
+                                
+        //Level7
+      if (!player.IsStunned || !player.IsSlowed) { return false;}
+        return true;
+                                    
+         
+    
+}
+}
